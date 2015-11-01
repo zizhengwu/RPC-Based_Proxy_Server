9fad223979f90bdc31a11f98b7847f9c09993178
Zizheng Wu <me@zizhengwu.com>
2015-11-01 12:35:48 -0500

    better inheritance

--- cache_implementation/BaseCache.h
+++ cache_implementation/BaseCache.h
@@ -19,14 +19,16 @@ public:
     };
 
     virtual void deduct_cache_size(const double& bytes_to_be_deducted);
-    virtual void insert_into_cache(const std::string &url, const std::string &content)=0;
-    virtual std::string retrieve_content_of_url(const std::string& url) const=0;
-    virtual bool check_if_exist_content_of_url(const std::string &url)=0;
+    virtual void insert_into_cache(const std::string& url, const std::string& content);
+    virtual std::string retrieve_content_of_url(const std::string& url) const;
+    virtual bool check_if_exist_content_of_url(const std::string& url);
+    virtual void update_cache_history(const std::string& url){};
     virtual void evict()=0;
 
 protected:
     double cache_size;
     double cache_size_maximum;
+    std::map<std::string, std::string> cache_map;
 };
 
 
--- cache_implementation/RandomCache.cpp
+++ cache_implementation/RandomCache.cpp
@@ -8,22 +8,3 @@ void RandomCache::evict() {
     std::cout <<  boost::format("swapped out %1% size %2%\n") % item->first % item->second.size();
     cache_map.erase(item);
 }
-
-void RandomCache::insert_into_cache(const std::string &url, const std::string &content) {
-    cache_map[url] = content;
-    cache_size += content.size();
-    while(get_cache_size() > cache_size_maximum)
-    {
-        evict();
-    }
-    std::cout << boost::format("current cache size: %1% bytes\n") % cache_size;
-};
-
-std::string RandomCache::retrieve_content_of_url(const std::string& url) const {
-    std::cout << boost::format("url: %1% hit\n") % url;
-    return (cache_map.at(url));
-};
-
-bool RandomCache::check_if_exist_content_of_url(const std::string &url) {
-    return !(cache_map.find(url) == cache_map.end());
-};
