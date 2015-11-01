8b9e95594a7e65249f37260b112ed3feba961858
Zizheng Wu <me@zizhengwu.com>
2015-11-01 12:39:50 -0500

    consistent message about size

--- cache_implementation/RandomCache.cpp
+++ cache_implementation/RandomCache.cpp
@@ -5,6 +5,6 @@ void RandomCache::evict() {
     auto item = cache_map.begin();
     std::advance(item, rand() % cache_map.size());
     deduct_cache_size(item->second.size());
-    std::cout <<  boost::format("swapped out %1% size %2%\n") % item->first % item->second.size();
+    std::cout <<  boost::format("swapped out %1% size %2%\n kb") % item->first % (item->second.size() / 1024);
     cache_map.erase(item);
 }
